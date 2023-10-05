73d56bb750e299a4e402ae6f2e1c18ee2a28e5c9
Max Baaten <m.p.j.baaten@tilburguniversity.edu>
2023-10-05 12:07:57 +0200

    I added a transform file for the second stage of our code

---
+++ src/data-preparation/Transform_Data_Relative.R
@@ -0,0 +1,72 @@
+#Set up
+library(tidyverse)
+
+# Define the list of city URLs
+citys <- list(
+  amsterdam = "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2023-09-03/visualisations/listings.csv",
+  berlin = "http://data.insideairbnb.com/germany/be/berlin/2023-06-22/visualisations/listings.csv",
+  brussels = "http://data.insideairbnb.com/belgium/bru/brussels/2023-06-24/visualisations/listings.csv",
+  london = "http://data.insideairbnb.com/united-kingdom/england/london/2023-06-08/visualisations/listings.csv",
+  paris = "http://data.insideairbnb.com/france/ile-de-france/paris/2023-06-06/visualisations/listings.csv"
+)
+
+# Loop through the city URLs and create data frames
+for (city_name in names(citys)) {
+  url <- citys[[city_name]]
+  
+  # Read the CSV data from the URL and store it in a data frame with the city name
+  assign(city_name, read.csv(url))
+}
+
+#code for private rooms segmentation
+city_names <- names(citys)
+
+# Loop through the city names and filter the corresponding data frames
+for (city_name in city_names) {
+  # Filter the data frame for "Private room" listings
+  assign(paste(city_name, "_private_rooms", sep = ""), 
+         get(city_name) %>%  # Access the original data frame by name
+           filter(room_type == "Private room"))
+  
+  # Print a message to indicate completion
+  cat("Filtered data for Private rooms in", city_name, "\n")
+}
+
+#slice the top and bottom 100 listings by price for each city
+
+# Loop through the city names
+for (city_name in city_names) {
+  # Access the data frame for private rooms in the current city
+  city_df <- get(paste(city_name, "_private_rooms", sep = ""))
+  
+  # Sort the data frame by the "price" column in ascending order
+  sorted_df <- city_df %>% arrange(price)
+  
+  # Create data frame for the combined 100 cheapest and 100 most expensive listings
+  combined_df <- bind_rows(
+    head(sorted_df, 100),  # Top 100 most expensive listings
+    tail(sorted_df, 100)   # Bottom 100 cheapest listings
+  )
+  
+  # Assign the combined data frame a unique name for each city
+  assign(paste(city_name, "_combined", sep = ""), combined_df)
+  
+  # Print a message to indicate completion
+  cat("Sorted data by price and sliced top and bottom 100 listings in", city_name, "by price\n")
+}
+
+# Loop through the city names
+for (city_name in city_names) {
+  # Access the combined data frame for the current city
+  combined_df <- get(paste(city_name, "_combined", sep = ""))
+  
+  # Add the 'is_expensive' column based on row number
+  combined_df <- combined_df %>%
+    mutate(is_expensive = ifelse(row_number() <= 100, 0, 1))
+  
+  # Assign the modified data frame back to its original name
+  assign(paste(city_name, "_combined", sep = ""), combined_df)
+  
+  # Print a message to indicate completion
+  cat("Created 'is_expensive' variable in", city_name, "combined data frame\n")
+}
