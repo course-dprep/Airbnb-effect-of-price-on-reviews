516c2c85e0978d73e5e610cbee32a549c634eddb
Max Baaten <m.p.j.baaten@tilburguniversity.edu>
2023-10-09 09:37:43 +0200

    Changed the transform file

--- src/data-preparation/Transform_Data_Relative.R
+++ src/data-preparation/Transform_Data_Relative.R
@@ -2,24 +2,29 @@
 library(tidyverse)
 
 # Define the list of city URLs
-citys <- list(
-  amsterdam = "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2023-09-03/visualisations/listings.csv",
-  berlin = "http://data.insideairbnb.com/germany/be/berlin/2023-06-22/visualisations/listings.csv",
-  brussels = "http://data.insideairbnb.com/belgium/bru/brussels/2023-06-24/visualisations/listings.csv",
-  london = "http://data.insideairbnb.com/united-kingdom/england/london/2023-06-08/visualisations/listings.csv",
-  paris = "http://data.insideairbnb.com/france/ile-de-france/paris/2023-06-06/visualisations/listings.csv"
+city_year <- list(
+  amsterdam_2022 = "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/visualisations/listings.csv",
+  berlin_2022 = "http://data.insideairbnb.com/germany/be/berlin/2022-12-21/visualisations/listings.csv",
+  brussels_2022 = "http://data.insideairbnb.com/belgium/bru/brussels/2022-12-24/visualisations/listings.csv",
+  london_2022 = "http://data.insideairbnb.com/united-kingdom/england/london/2022-12-10/visualisations/listings.csv",
+  paris_2022 = "http://data.insideairbnb.com/france/ile-de-france/paris/2022-12-10/visualisations/listings.csv",
+  amsterdam_2023 = "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2023-06-05/visualisations/listings.csv",
+  berlin_2023 = "http://data.insideairbnb.com/germany/be/berlin/2023-06-22/visualisations/listings.csv",
+  brussels_2023 = "http://data.insideairbnb.com/belgium/bru/brussels/2023-06-24/visualisations/listings.csv",
+  london_2023 = "http://data.insideairbnb.com/united-kingdom/england/london/2023-06-08/visualisations/listings.csv",
+  paris_2023 = "http://data.insideairbnb.com/france/ile-de-france/paris/2023-06-06/visualisations/listings.csv"
 )
 
 # Loop through the city URLs and create data frames
-for (city_name in names(citys)) {
-  url <- citys[[city_name]]
+for (city_name in names(city_year)) {
+  url <- city_year[[city_name]]
   
   # Read the CSV data from the URL and store it in a data frame with the city name
   assign(city_name, read.csv(url))
 }
 
 #code for private rooms segmentation
-city_names <- names(citys)
+city_names <- names(city_year)
 
 # Loop through the city names and filter the corresponding data frames
 for (city_name in city_names) {
