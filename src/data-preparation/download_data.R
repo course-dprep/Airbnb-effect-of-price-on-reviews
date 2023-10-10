2186adaa4ac566495c21440429834e3483b392dc
Max Baaten <m.p.j.baaten@tilburguniversity.edu>
2023-10-10 09:18:47 +0200

    New file for downloading the data

---
+++ src/data-preparation/download_data.R
@@ -0,0 +1,14 @@
+# Download Listings Files 2022
+download.file(url = 'http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2022-12-05/visualisations/listings.csv', destfile = '../../data/listings_amsterdam22.csv')
+download.file(url = 'http://data.insideairbnb.com/germany/be/berlin/2022-12-21/visualisations/listings.csv', destfile = '../../data/listings_berlin22.csv')
+download.file(url = 'http://data.insideairbnb.com/belgium/bru/brussels/2022-12-24/visualisations/listings.csv', destfile = '../../data/listings_brussels22.csv')
+download.file(url = 'http://data.insideairbnb.com/united-kingdom/england/london/2022-12-10/visualisations/listings.csv', destfile = '../../data/listings_london22.csv')
+download.file(url = 'http://data.insideairbnb.com/france/ile-de-france/paris/2022-12-10/visualisations/listings.csv', destfile = '../../data/listings_paris22.csv')
+
+# Download Listings Files 2023
+download.file(url = 'http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2023-06-05/visualisations/listings.csv', destfile = '../../data/listings_amsterdam23.csv') 
+download.file(url = 'http://data.insideairbnb.com/germany/be/berlin/2023-06-22/visualisations/listings.csv', destfile = '../../data/listings_berlin23.csv') 
+download.file(url = 'http://data.insideairbnb.com/belgium/bru/brussels/2023-06-24/visualisations/listings.csv', destfile = '../../data/listings_brussels23.csv')
+download.file(url = 'http://data.insideairbnb.com/united-kingdom/england/london/2023-06-08/visualisations/listings.csv', destfile = '../../data/listings_london23.csv') 
+download.file(url = 'http://data.insideairbnb.com/france/ile-de-france/paris/2023-06-06/visualisations/listings.csv', destfile = '../../data/listings_paris23.csv')
+
