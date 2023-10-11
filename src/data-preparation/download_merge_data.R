4e54f2fe2a417188d7943f659c0513e1f8d2bfd8
Max Baaten <m.p.j.baaten@tilburguniversity.edu>
2023-10-11 17:35:13 +0200

    Changes to source code

--- src/data-preparation/download_merge_data.R
+++ src/data-preparation/download_merge_data.R
@@ -58,21 +58,3 @@ dir.create('../../gen/data-preparation/temp')
 write.csv(all_data, file = "../../gen/data-preparation/temp/data_framed.csv", fileEncoding = "UTF-8",row.names=FALSE )
 
 cat("Combined data saved as CSV:\n")
-
-# Set up
-library(tidyverse)
-
-# Load the combined data frame
-df_framed <- read_csv("../../gen/data-preparation/temp/data_framed.csv")
-
-df_framed<- df_framed%>% filter(room_type == "Private room")
-filtered_df_cheap <- df_framed %>%
-  group_by(city_code) %>%
-  slice_min(order_by = price, n = 100)
-
-filtered_df_expensive <- df_framed %>%
-  group_by(city_code) %>%
-  slice_max(order_by = price, n = 100)
-
-cleaned_data_final <- bind_rows(filtered_df_cheap, filtered_df_expensive)
-write.csv(cleaned_data_final, file = "../../gen/data-preparation/temp/cleaned_data_final.csv", fileEncoding = "UTF-8",row.names=FALSE )
